60bea48326a3750eb820bf98b10eb5e52d39b1ce
Yogesh3 <ymehta630@gmail.com>
2020-02-16 18:05:07 -0700

    Need to add meshgrid to vectorize cib.py

--- cib.py
+++ cib.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 from matplotlib import pyplot as plt
 import sympy as sym
@@ -12,18 +6,11 @@ from astropy import units as u
 from astropy.modeling.blackbody import blackbody_nu
 from scipy.optimize import fsolve
 import pdb
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# # Code without Full SED
-
-# In[36]:
-
 
 def blackbody(v, T):
-    #Gives a blackbody spectrum as a function of frequency and temperature
+    # Gives a blackbody spectrum as a function of frequency and temperature
 
-    # Defining physical constants
+    #Defining physical constants
     c = const.c.cgs.value
     h = const.h.cgs.value
     k_B = const.k_B.cgs.value
@@ -33,14 +20,16 @@ def blackbody(v, T):
 def boltzmann(x, T):
     # Returns the Boltzmann factor for a given temperature and x
 
-    # Defining physical constants
+    #Defining physical constants
     c = const.c.cgs.value
     h = const.h.cgs.value
     k_B = const.k_B.cgs.value
 
     return np.exp(h*x / (k_B*T))
 
-def sed(nu_obs, z, beta=1.6, alpha=0.2):
+def capitalTheta(nu_obs, z, beta, alpha):
+    # Rest frame SED
+
     #Undoing Redshift: from Observing to Original
     nu = nu_obs * (1+z)
     temp_obs = 20.7                        # effective dust temperature at z=0
@@ -48,24 +37,29 @@ def sed(nu_obs, z, beta=1.6, alpha=0.2):
 
     return nu * blackbody(nu, temp_array)
 
-def phi(z, delta=2.4):
+def capitalPhi(z, delta):
     # Redshift dependent global normalization
     return (1+z)**delta
 
-def sigma(M,z):
-    #Halo mass dependance of glaxy luminosity
+def capitalSigma(M):
+    # Halo mass dependance of galaxy luminosity
 
-    logM_eff = 12.3  # M_eff = log10(mass peak of specific IR emissivity) in solar masses
-    sigma = 0.3      # standard deviation of the Gaussian
+    logM_eff = 12.3  # logM_eff = log10(mass peak of specific IR emissivity) in solar masses
+    sigma2 = 0.3      # (standard deviation)^2 of the Gaussian
 
-    return M/np.sqrt(2*np.pi*sigma**2) * np.exp(- (np.log10(M)-logM_eff)**2 / (2*sigma**2))
+    return M/np.sqrt(2*np.pi*sigma2) * np.exp(- (np.log10(M)-logM_eff)**2 / (2*sigma2))
 
-def luminosity(M, z, v_obs):
-    return L_o * sigma(M,z) * phi(z) * sed(v_obs, z)
+def luminosity(M, z, v_obs, a=0.2, b=1.6, d=2.4):
+    #Data Dictionary
+    #   a = alpha
+    #   b = beta
+    #   d = delta
 
-#Input data
-nu_obs = np.array([353.0e9])
-redshifts = np.array([0, 1, 2])
+    return capitalSigma(M) * capitalPhi(z, d) * capitalTheta(v_obs, z, b, a)
 
-#Run the code
-luminosity(M, redshifts, nu_obs)
+# #Input data
+# nu_obs = np.array([353.0e9])
+# redshifts = np.array([0, 1, 2])
+#
+# #Run the code
+# luminosity(M, redshifts, nu_obs)
