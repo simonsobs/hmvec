8ada6a62af48e302e0f6e30b276ade84d8e338f9
Yogesh <Yogesh@MacAir.local>
2020-05-22 15:46:17 -0700

    First change to model dcdz

--- examples/cib_example.py
+++ examples/cib_example.py
@@ -3,10 +3,10 @@ import hmvec as hm
 import matplotlib.pyplot as plt
 
 #Grid for Integration
-Nz = 100                                 # num of redshifts
-Nm = 100                                 # num of masses
-Nk = 10001                                # num of wavenumbers
-redshifts = np.linspace(0.01, 3, Nz)             # redshifts
+Nz = 600                                 # num of redshifts
+Nm = 500                                 # num of masses
+Nk = 1000                                # num of wavenumbers
+redshifts = np.linspace(0.01, 4.5, Nz)             # redshifts
 masses = np.geomspace(1e11, 1e15, Nm)           # masses
 ks = np.geomspace(1e-3, 100, Nk)               # wavenumbers
 frequencies = np.array([271.0])
@@ -14,7 +14,34 @@ frequencies = np.array([271.0])
 #Initialize Halo Model
 hcos = hm.HaloModel(redshifts, ks, ms=masses)
 
-#Get Power Spectra
-for i in frequencies:
-    Pjj_2h = hcos.get_power_2halo("cib", "cib", nu_obs=frequencies[i])  # P(z,k)
+#Get 3D Power Spectra
+Pjj_2h = hcos.get_power_2halo("cib", "cib", nu_obs=frequencies[0])  # P(z,k)
+
+#Limber Integrals
+Nl = 1000
+ells = np.arange(1000, num=Nl)
+Cii, Cii_integrand = hcos.C_ii(ells, redshifts, ks, Pjj_2h, dcdzflag=True)
+
+#Plot Cii
+plt.loglog(ells, Cii)
+plt.xlabel(r'$\ell$')
+plt.ylabel(rf'$C^{{ {frequencies[0]:0.0f} \;x\; {frequencies[0]:0.0f} }}_\ell$');
+plt.savefig('cii.pdf', dpi=500, bbox_inches='tight')
+
+#Plot dC/dz (z)
+test_ells = np.array([100,300, 500, 1000])
+plt.figure(figsize=(10,7))
+for ell in test_ells:
+    #Get index
+    i = np.where(abs(ell - ells) <= 1)[0][0]
+
+    #Spectra
+    plt.semilogy(redshifts, Cii_integrand[:, i], label=rf"$\ell = {ells[i]:0.0f}$")
+
+    #Gravy
+    plt.xlabel(r'$z$')
+    plt.ylabel(r'$dC_{II} / dz$')
+    plt.title(rf'$\nu$ = {frequencies[0]}')
+    plt.legend()
+plt.savefig('dCdz_ii.png', dpi=500, bbox_inches='tight');
 
