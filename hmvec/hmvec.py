f902a3a8b5ee0ebe8497d7cbf057d4578226839b
Yogesh <Yogesh@MacAir.local>
2020-06-20 23:03:20 -0700

    Gaussian over linear space

--- hmvec/hmvec.py
+++ hmvec/hmvec.py
@@ -581,7 +581,7 @@ class HaloModel(Cosmology):
         fquad = np.zeros(len(self.ms))
         quaderr = np.zeros(len(self.ms))
         for i, centralM in enumerate(self.ms):
-            fquad[i], quaderr[i] = quad(gaussinteg, np.log10(self.ms[0]), np.log10(centralM), args=(np.log10(centralM),))
+            fquad[i], quaderr[i] = quad(integ, self.ms[0], centralM, args=(centralM,))
         end = time.time()
 
         timestring += f'Gaussian Quadrature 2 time (s): {end-start} \n'
