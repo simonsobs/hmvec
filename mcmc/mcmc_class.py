fe9cd73d4a6d28b4f42f04ea1fb16b39ec5e98b8
Yogesh3 <ymehta630@gmail.com>
2021-02-09 16:14:07 -0800

    transitioning to niagara

--- mcmc/mcmc_class.py
+++ mcmc/mcmc_class.py
@@ -1,9 +1,8 @@
 import numpy as np
 import hmvec as hm
-import matplotlib.pyplot as plt
-import matplotlib
 from cobaya.theory import Theory
 from cobaya.likelihood import Likelihood
+import getdata
 
 class PowerSpectrum(Theory):
     params = {'alpha':None, 'beta':None, 'gamma':None, 'delta':None, 'Td_o':None, 'logM_eff':None, 'L_o':None, 'freq':None}
@@ -18,38 +17,36 @@ class PowerSpectrum(Theory):
         self.ks = np.geomspace(1.0e-3, 100.0, Nk)              # wavenumbers
         self.ells = np.linspace(10, 2000, 200)
 
-        #Initialize Halo Model 
+        #Initialize Halo Model
         self.hcos = hm.HaloModel(self.redshifts, self.ks, ms=self.masses)
+        
+        #Autocorrelation: 1 Freq
+        self.autofreq = getdata.freq()
 
     def get_can_provide(self):
         return ['Cl']
 
-    def calculate(self, state, want_derived=True, **params):
-        #Extract Just CIB Parameters
-        cibparams = dict()
-        for key, value in params.items():
-            if key == 'freq':
-                continue
-            cibparams[key] = value
-
-        #Set New Parameters
-        self.hcos.set_cibParams('custom', **cibparams)
-
+    def calculate(self, state, want_derived=True, **params):  
+        #Set parameters
+        self.hcos.set_cibParams('planck13', **params)
+        
         #Power Spectrum
-        Pjj_tot = self.hcos.get_power("cib", "cib", nu_obs= params['freq'], satmf= 'tinker') 
+        Pjj_tot = self.hcos.get_power("cib", "cib", nu_obs= self.autofreq, satmf= 'tinker') 
         state['Cl'] = self.hcos.C_ii(self.ells, self.redshifts, self.ks, Pjj_tot, dcdzflag= False)
 
 class ChiSqLikelihood(Likelihood):
-    params = {'covariance':None, 'data':None}
-
+    def initialize(self):
+        self.cov = getdata.covariance()
+        self.data = getdata.datapoints()
+    
     def get_requirements(self):
         return ['Cl']
 
     def logp(self, **param_values):
         Cl = self.provider.get_result('Cl')
-        dev = param_values['data'][:,1] - Cl
-
-        chisquared = dev.T @ np.linalg.inv(param_values['covariance']) @ dev
+        dev = self.data[:,1] - Cl
+                
+        chisquared = dev.T @ np.linalg.inv(self.cov) @ dev
 
         return -1.0/2.0 * chisquared
         
